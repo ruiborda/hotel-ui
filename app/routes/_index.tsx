7b7c8a6325e99239773b2cbfac785f9baa2eb725
ruiborda <bordarui@gmail.com>
2024-10-05 20:37:12 -0500

    init

---
+++ app/routes/_index.tsx
@@ -0,0 +1,16 @@
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+        <h1 className="text-4xl font-bold">Pagina inicio de sesión</h1>
+    </div>
+  );
+}
