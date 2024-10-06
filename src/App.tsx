3c9c4758e0b1e5d2db07a9894dfe21a1f7a6d884
Ricardo Vargas Vargas <vargasvargas.ric@gmail.com>
2024-10-05 20:38:37 -0500

    add: boton

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">
-          hola soy rui
+          hola soy RICARDO <br /> <br />
+          <button className="text-2xl font-bold underline">DAME CLICK</button>
       </h1>
     </>
   );
